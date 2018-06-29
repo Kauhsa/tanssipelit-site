ab47cfa7a6c070e1fcd8c2061450ce9117d9d3fe
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2018-06-29 21:00:04 +0300

    Wrapper for news

--- src/templates/news.js
+++ src/templates/news.js
@@ -2,14 +2,17 @@ import { graphql } from "gatsby";
 import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../components/Layout";
+import Content from "../components/Content";
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data;
   return (
     <Layout>
-      <Helmet title={post.frontmatter.title} />
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Content>
+        <Helmet title={post.frontmatter.title} />
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Content>
     </Layout>
   );
 }
