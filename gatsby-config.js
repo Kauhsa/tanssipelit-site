9a695bffe546111b892b1ee71b5457615cd9f0e9
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2018-07-20 17:39:14 +0300

    Upgrade packages, install gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,14 @@ module.exports = {
       }
     },
 
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      }
+    },
+
     "gatsby-plugin-netlify"
   ]
 };
